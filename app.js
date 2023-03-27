c394f8b48760795239945267fc726b6d401738e1
Hafiz Caniago <hafizcode02@gmail.com>
2023-03-27 11:03:37 +0700

    add body parser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const app = express();
-async function init(){
+async function init() {
+    app.use(bodyparser.json({ limit: '50mb' }));
+    app.use(bodyparser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
     const appRouting = require('./modules');
     const appModules = new appRouting(app);
     appModules.init();
