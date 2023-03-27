ffa1a9de54ed7b71acc13550409a77b4091b1238
Hafiz Caniago <hafizcode02@gmail.com>
2023-03-27 10:35:56 +0700

    add info server running

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 const server = require('http').Server(app);
 server.listen(1234, 'localhost', () => {
-    console.log(`server running successfuly`);
+    console.log(`server running successfuly at http://localhost:1234`);
 });
