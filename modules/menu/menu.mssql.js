615abef03a3fe7950ce0a4482833b0b1c212284d
Hafiz Caniago <hafizcode02@gmail.com>
2023-03-27 10:36:17 +0700

    replace default id with hashids

--- modules/menu/menu.mssql.js
+++ modules/menu/menu.mssql.js
@@ -1,10 +1,21 @@
+const Hashids = require('hashids/cjs')
+
 const mssqlCon = require('../../dbconnection');
+const hashids = new Hashids('lks23', 16)
 
 class MenuMSSql {
-    async getAllMenus(){
+    async getAllMenus() {
         const conn = await mssqlCon.getConn();
-        const res = await conn.request().query('SELECT * FROM MsMenu');
-        return res.recordset;
+        const res = await conn.request().query('SELECT Id, Name, Price FROM MsMenu');
+        const result = res.recordset;
+        const expectedResult = Object.keys(result).map(key => {
+            let data = {};
+            data = result[key];
+            data.Id = hashids.encode(data.Id);
+            return data;
+        });
+
+        return expectedResult;
     }
 }
 module.exports = new MenuMSSql();
